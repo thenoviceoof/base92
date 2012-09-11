7c9c53068e45fb6550bed2e49c1fa211d2df9d49
Nathan Hwang <thenoviceoof@gmail.com>
2012-09-10 21:39:12 -0400

    Change name back to base92

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
-base91
+base92
 ================================================================================
 A little python library for encoding byte-strings into strings easily
 typeable on a standard US 101-key keyboard.
 
 DESIGN DECISIONS
 --------------------------------------------------------------------------------
-We are going to try and be clever, and leave out:
-   ~, `, "
+We are going to try and be clever, and leave out some characters from
+the general encoding scheme:
+
+    ~, `, "
 
 The ` and " are just too close to ' for comfort
 
 However, we'll use ~ for denoting an end
 
 There are 94 printable ascii characters, so we end up with 91
-characters, or 6.5 bits per character
+characters, or 6.5 bits per character.
+
+Once we include the ~, then we have 92 characters: hence, base92
--- base92.py
+++ base92.py
@@ -5,7 +5,7 @@
 # - Nathan Hwang (thenoviceoof)
 
 '''
-base91: a library for encoding things
+base92: a library for encoding byte strings
 
 >>> x = encode('hello world')
 >>> x
@@ -19,23 +19,26 @@ base91: a library for encoding things
 >>> decode(y)
 '^\\xb6;\\xbb\\xe0\\x1e\\xee\\xd0\\x93\\xcb"\\xbb\\x8fZ\\xcd\\xc3'
 '''
+
+import math
+
 __version__ = (0, 0, 1)
 
-def base91_chr(val):
+def base92_chr(val):
     '''
     Map an integer value <91 to a char
 
-    >>> base91_chr(0)
+    >>> base92_chr(0)
     '!'
-    >>> base91_chr(1)
+    >>> base92_chr(1)
     '#'
-    >>> base91_chr(61)
+    >>> base92_chr(61)
     '_'
-    >>> base91_chr(62)
+    >>> base92_chr(62)
     'a'
-    >>> base91_chr(90)
+    >>> base92_chr(90)
     '}'
-    >>> base91_chr(91)
+    >>> base92_chr(91)
     Traceback (most recent call last):
         ...
     ValueError: val must be in [0, 91)
@@ -49,24 +52,24 @@ def base91_chr(val):
     else:
         return chr(ord('a') + val - 62)
 
-def base91_ord(val):
+def base92_ord(val):
     '''
     Map a char to an integer
 
-    >>> base91_ord('!')
+    >>> base92_ord('!')
     0
-    >>> base91_ord('#')
+    >>> base92_ord('#')
     1
-    >>> base91_ord('_')
+    >>> base92_ord('_')
     61
-    >>> base91_ord('a')
+    >>> base92_ord('a')
     62
-    >>> base91_ord('}')
+    >>> base92_ord('}')
     90
-    >>> base91_ord(' ')
+    >>> base92_ord(' ')
     Traceback (most recent call last):
         ...
-    ValueError: val is not a base91 character
+    ValueError: val is not a base92 character
     '''
     num = ord(val)
     if val == '!':
@@ -76,25 +79,25 @@ def base91_ord(val):
     elif ord('a') <= num and num <= ord('}'):
         return num - ord('a') + 62
     else:
-        raise ValueError('val is not a base91 character')
+        raise ValueError('val is not a base92 character')
 
-def base91_encode(bytstr):
+def base92_encode(bytstr):
     '''
     Take a byte-string, and encode it in base 91
 
-    >>> base91_encode("")
+    >>> base92_encode("")
     '~'
-    >>> base91_encode("\\x00")
+    >>> base92_encode("\\x00")
     '!!'
-    >>> base91_encode("\x01")
+    >>> base92_encode("\x01")
     '!B'
-    >>> base91_encode("\xff")
+    >>> base92_encode("\xff")
     '|_'
-    >>> base91_encode("aa")
+    >>> base92_encode("aa")
     'D8-9~'
-    >>> base91_encode("hello world")
+    >>> base92_encode("hello world")
     'Fc_$aOTdKnsM#+'
-    >>> base91_encode("aaaaaaaaaaaaa")
+    >>> base92_encode("aaaaaaaaaaaaa")
     'D81RPya.)hgNA(%s'
     '''
     # always encode *something*, in case we need to avoid empty strings
@@ -114,45 +117,44 @@ def base91_encode(bytstr):
             bitstr += '{:08b}'.format(ord(bytstr[0]))
             bytstr = bytstr[1:]
         i = int(bitstr[:13], 2)
-        resstr += base91_chr(i / 91)
-        resstr += base91_chr(i % 91)
+        resstr += base92_chr(i / 91)
+        resstr += base92_chr(i % 91)
         bitstr = bitstr[13:]
     if bitstr:
         bitstr += '0' * (13 - len(bitstr))
         i = int(bitstr, 2)
-        resstr += base91_chr(i / 91)
-        resstr += base91_chr(i % 91)
+        resstr += base92_chr(i / 91)
+        resstr += base92_chr(i % 91)
     if lop:
         resstr += '~'
     return resstr
 
-import math
-def base91_decode(bstr):
+def base92_decode(bstr):
     '''
-    Take a base91 encoded string, convert it back to a byte-string
+    Take a base92 encoded string, convert it back to a byte-string
 
-    >>> base91_decode("")
+    >>> base92_decode("")
     ''
-    >>> base91_decode("~")
+    >>> base92_decode("~")
     ''
-    >>> base91_decode("!!")
+    >>> base92_decode("!!")
     '\\x00'
-    >>> base91_decode("!B")
+    >>> base92_decode("!B")
     '\\x01'
-    >>> base91_decode("|_")
+    >>> base92_decode("|_")
     '\\xff'
-    >>> base91_decode("D8-9~")
+    >>> base92_decode("D8-9~")
     'aa'
-    >>> base91_decode('Fc_$aOTdKnsM#+')
+    >>> base92_decode('Fc_$aOTdKnsM#+')
     'hello world'
-    >>> base91_decode("D81RPya.)hgNA(%s")
+    >>> base92_decode("D81RPya.)hgNA(%s")
     'aaaaaaaaaaaaa'
     '''
     bitstr = ''
     resstr = ''
     # we always have pairs of characters
     for i in range(len(bstr)/2):
-        x = base91_ord(bstr[2*i])*91 + base91_ord(bstr[2*i+1])
+        x = base92_ord(bstr[2*i])*91 + base92_ord(bstr[2*i+1])
         bitstr += "{:013b}".format(x)
         while 8 <= len(bitstr):
             resstr += chr(int(bitstr[0:8], 2))
@@ -161,6 +163,9 @@ def base91_decode(bstr):
     if bstr and bstr[-1] == '~':
         resstr = resstr[:-1]
     return resstr
+
+encode = base92_encode
+decode = base92_decode
 
 if __name__ == "__main__":
     import doctest
