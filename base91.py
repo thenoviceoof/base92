2c6680f120e05e49e44884df6380feedeb825f9f
Nathan Hwang <thenoviceoof@gmail.com>
2012-09-10 21:39:32 -0400

    Add beerware license

--- base91.py
+++ base91.py
@@ -1,7 +1,12 @@
+# THE BEERWARE LICENSE (Revision 42):
+# <thenoviceoof> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you
+# think this stuff is worth it, you can buy me a beer in return
+# - Nathan Hwang (thenoviceoof)
+
 '''
 base91: a library for encoding things
 
-BEERWARE LICENSE
 '''
 __version__ = (0, 0, 1)
 
