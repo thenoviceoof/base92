ac9a78da4fc92214c539c02d957d205318b0b09d
Nathan Hwang <thenoviceoof@gmail.com>
2012-09-23 10:32:34 -0400

    Start creating the c lib

---
+++ c/base92.c
@@ -0,0 +1,9 @@
+// THE BEERWARE LICENSE (Revision 42):
+// <thenoviceoof> wrote this file. As long as you retain this notice you
+// can do whatever you want with this stuff. If we meet some day, and you
+// think this stuff is worth it, you can buy me a beer in return
+// - Nathan Hwang (thenoviceoof)
+
+int main() {
+        return 0;
+}
