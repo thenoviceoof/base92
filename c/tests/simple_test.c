ffbd1d22fb03dd0d96c69e735a7d84cc69a7fe0d
Nathan Hwang <thenoviceoof@gmail.com>
2012-10-07 01:44:59 -0400

    Add another test, with temp output

---
+++ c/tests/simple_test.c
@@ -0,0 +1,20 @@
+// THE BEERWARE LICENSE (Revision 42):
+// <thenoviceoof> wrote this file. As long as you retain this notice you
+// can do whatever you want with this stuff. If we meet some day, and you
+// think this stuff is worth it, you can buy me a beer in return
+// - Nathan Hwang (thenoviceoof)
+
+#include <base92.h>
+#include <stdio.h>
+
+int main() {
+        char *str = "c";
+
+        printf("Get some input up in here\n");
+        printf("%s\n", base92encode(str, 1));
+        if(strcmp(base92encode(str, 1), "Dm") != 0)
+                exit(1);
+        if(strcmp(base92decode("Dm"), str) != 0)
+                exit(1);
+        return 0;
+}
