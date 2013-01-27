c3ebd17d307cba3373fdb93c8eb836794c8126bb
Nathan Hwang <thenoviceoof@gmail.com>
2013-01-26 19:44:37 -0500

    Make base92 an actual module, not just a lonely file

---
+++ python/base92/__init__.py
@@ -0,0 +1,14 @@
+'''
+Import routines from base92.base92 for manipulating base92 encoded strings.
+
+Example:
+
+>>> from base92 import encode, decode, b92encode, b92decode
+>>> x = encode('hello world')
+>>> x
+'Fc_$aOTdKnsM*k'
+>>> decode(x)
+'hello world'
+'''
+
+from base92.base92 import encode, decode, b92encode, b92decode
