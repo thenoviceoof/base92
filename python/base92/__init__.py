a9897920da53cdb2ec315a85edef4c5e46916232
backbord <tim@strazny.de>
2017-01-27 09:27:46 +0100

    cleanup

--- python/base92/__init__.py
+++ python/base92/__init__.py
@@ -1,4 +1,4 @@
-'''
+"""
 Import routines from base92.cbase92 or base92.base92 for manipulating base92 encoded strings.
 
 Example:
@@ -9,9 +9,18 @@ Example:
 'Fc_$aOTdKnsM*k'
 >>> decode(x)
 'hello world'
-'''
+"""
+
+from . import base92
+
 try:
-    from .cbase92 import encode, decode, b92encode, b92decode, __version__
+    from . import cbase92
+    preferred_base92 = cbase92
 except (ImportError, OSError) as e:
     print('Falling back to base92 python backend due to: {}'.format(e))
-    from .base92 import encode, decode, b92encode, b92decode, __version__
+    preferred_base92 = base92
+    cbase92 = None
+
+encode = b92encode = preferred_base92.encode
+decode = b92decode = preferred_base92.decode
+__version__ = base92.__version__
