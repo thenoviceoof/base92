52cb0d3efcca43276b5f04e04bcc2c2af141668e
Nathan Hwang <thenoviceoof@gmail.com>
2025-06-02 17:49:38 -0400

    Python: update size benchmark script to use proper base85 and new base92 structure

--- python/performance/size.py
+++ python/performance/size.py
@@ -1,12 +1,17 @@
 # Used to derive the size advantage of base92 over base64/base85 which
 # is used in the README.
 import base64
-import base85
 from pprint import pprint
-sd = [(len(base64.b64encode('a'*i)),
-       len(base85.b85encode('a'*i)),
-       len(encode('a'*i)))
-      for i in range(1,128)]
+import base92
+
+sd = [
+    (
+        len(base64.b64encode(b"a" * i)),
+        len(base64.a85encode(b"a" * i)),
+        len(base92.b92encode(b"a" * i)),
+    )
+    for i in range(1, 128)
+]
 pprint(sd)
-print(sum(a-c for a,b,c in sd)/float(len(sd)))
-print(sum(b-c for a,b,c in sd)/float(len(sd)))
+print(sum(a - c for a, b, c in sd) / float(len(sd)))
+print(sum(b - c for a, b, c in sd) / float(len(sd)))
